8b2b0fc593232808721b149a2ace9eaa99ffeaef
manuchong11 <121359680+manuchong11@users.noreply.github.com>
2026-03-06 22:35:04 +0000

    updated classes

--- src/main/java/com/example/entities/Vacation.java
+++ src/main/java/com/example/entities/Vacation.java
@@ -1,5 +1,29 @@
 package com.example.entities;
 
 public class Vacation {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long countryId;
+    
+    @Column(name = "package_price")
+    private String country;
+
+    @Column(name = "party_size")
+    @CreationTimestamp
+    private Date createDate;
+
+    @Column(name = "status")
+    @UpdateTimestamp
+    private Date lastUpdate;
+
+    @Column(name = "order_tracking_number")
+    private;
+
+    @Column(name = "create_date")
+    private;
+
+    @Column(name = "last_update")
+    private;
 
 }
