5dcc6c9b10efc13e0299c4b35ae3cb021fc70e72
manuchong11 <121359680+manuchong11@users.noreply.github.com>
2026-03-06 02:17:34 +0000

    updated customers file

--- src/main/java/com/example/entities/Customers.java
+++ src/main/java/com/example/entities/Customers.java
@@ -2,8 +2,6 @@ package com.example.entities;
 
 import java.util.Date;
 
-import javax.annotation.processing.Generated;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
