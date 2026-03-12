81534b850d433d147ef8b8b3720b2cc10bacf43f
manuchong11 <121359680+manuchong11@users.noreply.github.com>
2026-03-12 21:43:40 +0000

    renamed repositories

--- src/main/java/com/example/services/CheckoutServiceImpl.java
+++ src/main/java/com/example/services/CheckoutServiceImpl.java
@@ -1,6 +1,19 @@
 package com.example.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CheckoutServiceImpl implements CheckoutService {
+    private CustomerRepository customerRepository;
+    private CartItemRepository cartItemRepository;
+    private CartRepository cartRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository, CartItemRepository cartItemRepository,
+            CartRepository cartRepository) {
+        this.customerRepository = customerRepository;
+        this.cartItemRepository = cartItemRepository;
+        this.cartRepository = cartRepository;
+    }
 
     @Override
     public PurchaseResponse placeOrder(Purchase purchase) {
